0ca9f176b2afb15f2d1757747c7382bf084d1ea6
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-25 12:46:36 +0100

    Update 1.py

---
+++ coded solutions/1.py
@@ -0,0 +1,10 @@
+
+
+
+def solve_1(pattern): #solve given a pattern
+    # @param pattern --> matrix
+    # @returns matrix
+    pass
+
+def generate_1(): #generate a new pattern and the answer
+    pass
