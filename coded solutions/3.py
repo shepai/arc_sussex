b7e1f9fdf07cbfa8f94a9086af4694ac7cb23b1d
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-26 09:39:30 +0100

    Update 3.py

--- coded solutions/3.py
+++ coded solutions/3.py
@@ -3,7 +3,7 @@ import numpy as np
 
 colours={'black':0,'blue':1,'red':2,'green':3,'yellow':4,'gray':5,'pink':6,'orange':7,'light-blue':8,'maroon':9}
 
-def solve_1(pattern): #solve given a pattern
+def solve_3(pattern): #solve given a pattern
     # @param pattern --> matrix
     # @returns matrix
     assert pattern.shape==(6,3), "Incorrect input size "+str(pattern.shape)
@@ -11,7 +11,7 @@ def solve_1(pattern): #solve given a pattern
     new[0:6,]=pattern
     new[6:,0:3]=pattern[2:5,:]
     return new
-def generate_1(): #generate a new pattern and the answer
+def generate_3(): #generate a new pattern and the answer
     c=np.random.randint(0,len(list(colours.keys()))-1)
     random_matrix_numpy = np.random.randint(2, size=(6, 3))
     random_matrix_numpy[random_matrix_numpy==1]=c
@@ -45,6 +45,6 @@ def display(m1,m2):
     plt.tight_layout()
     plt.show()
 
-m1=generate_1()
+"""m1=generate_1()
 m2=solve_1(m1)
-display(m1,m2)
+display(m1,m2)"""
