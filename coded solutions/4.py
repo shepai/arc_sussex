ab4d1835f3d411d38daf0f65360f1d34d70d2b6a
Dexter Shepherd <dextershepherd@hotmail.co.uk>
2024-09-26 10:11:13 +0100

    unfinished basic outline

--- coded solutions/4.py
+++ coded solutions/4.py
@@ -7,8 +7,10 @@ def solve_4(pattern): #solve given a pattern
     # @param pattern --> matrix
     # @returns matrix
     new=np.zeros_like(pattern)
-    new[0:6,]=pattern
-    new[6:,0:3]=pattern[2:5,:]
+    points=np.argwhere(pattern>0)[:,]
+    furthest=np.max(np.argwhere(pattern>0)[:,])
+    print(points)
+    print(furthest)
     return new
 
 def generate_4(): #generate a new pattern and the answer
@@ -16,10 +18,18 @@ def generate_4(): #generate a new pattern and the answer
     canvas = np.zeros((np.random.randint(8,20), np.random.randint(8,20)))
     canvas=draw_shape(canvas)
     canvas[canvas==1]=c
+    c1=0
+    t=1
+    if c1==t: t=2 #prevent being same colour
+    if np.random.randint(0,3)==3: #random chance of another shape
+        pass
+    c=np.random.randint(0,len(list(colours.keys()))-1)
+    canvas=add_shape(canvas)
+    canvas[canvas==t]=c
     return canvas
 
 
 
-"""m1=generate_4()
+m1=generate_4()
 m2=solve_4(m1)
-display(m1,m2)"""
+display(m1,m2)
